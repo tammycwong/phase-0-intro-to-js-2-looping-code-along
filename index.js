12a073634501fcec8203274089fdc3b529f2139d
Tammy <tammycwong@gmail.com>
2021-03-01 17:36:26 -0500

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = [];
+const event = [];
+let message = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+let num = []
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num-=1;
+    }
+}
